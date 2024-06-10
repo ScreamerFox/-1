d0501b13e2f339d3eb0a403a8b491a1b3bbb626f
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-06-10 22:09:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ is_student = True
 print(is_student)
 
 print(f'Меня зовут {name}, мне {age} лет, но через 21 год мне будет {new_age} лет!')
+
+if is_student == True:
+    print('А ещё я студент!')
+else:
+    print('А может и нет))')
