67d2b63c91c3c96d1e772a3caa60cfe749f51eee
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-06-10 22:06:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "Eugene"
+print(name)
+age = 29
+print(age)
+new_age = age + 21
+print(age)
+is_student = True
+print(is_student)
+
+print(f'Меня зовут {name}, мне {age} лет, но через 21 год мне будет {new_age} лет!')
